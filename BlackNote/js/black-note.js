7795826240756541dc55572321d7e28f7543466d
KiyonoRin <Lbiebest@outlook.com>
2024-01-16 18:30:01 +0800

    chroe: 添加任务内容不能为空

--- BlackNote/js/black-note.js
+++ BlackNote/js/black-note.js
@@ -1,19 +1,23 @@
 import { createApp, ref, nextTick } from "https://cdn.jsdelivr.net/npm/vue@3/dist/vue.esm-browser.js";
 const todoapp = createApp({
     setup() {
-        const list = [
+        // 参数部分
+        const items = ref([
             "写代码",
             "吃饭饭",
             "睡觉觉"
-        ];
-        const items = ref(list);
+        ]);
         const inputValue = ref("");
 
-        // function
+        // 函数部分
         const add = () => {
             // 获取元素添加进 items
-            items.value.push(inputValue.value);
-            inputValue.value = "";
+            if (inputValue.value == "") {
+                alert("请输入内容");
+            } else {
+                items.value.push(inputValue.value);
+                inputValue.value = "";
+            }
         };
 
         const remove = (index) => {
@@ -23,9 +27,8 @@ const todoapp = createApp({
 
         const clear = () => {
             // 删除所有元素
-            items.value = [];  
+            items.value = [];
         };
-        
 
         return {
             items, inputValue,
