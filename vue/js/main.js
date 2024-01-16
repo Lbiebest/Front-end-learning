d3535be8bf83c130770e080940cd252a023c0d59
KiyonoRin <Lbiebest@outlook.com>
2024-01-16 16:45:24 +0800

    thirty: vue 指令

---
+++ vue/js/main.js
@@ -0,0 +1,5 @@
+// main.js
+import * as Vue from 'https://cdn.jsdelivr.net/npm/vue@3/dist/vue.esm-browser.js';
+// import * as Vue from 'https://cdn.jsdelivr.net/npm/vue@3/dist/vue.global.prod.js';
+  
+export { Vue };
