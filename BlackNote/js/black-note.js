d5707af55f7b255e89761ea35e8bc22c3fefb796
KiyonoRin <Lbiebest@outlook.com>
2024-01-16 16:44:35 +0800

    second: 小黑记事本

---
+++ BlackNote/js/black-note.js
@@ -0,0 +1,35 @@
+import { createApp, ref } from "https://cdn.jsdelivr.net/npm/vue@3/dist/vue.esm-browser.js";
+const todoapp = createApp({
+    setup() {
+        const list = [
+            "写代码",
+            "吃饭饭",
+            "睡觉觉"
+        ];
+        const items = ref(list);
+        const inputValue = ref("");
+
+        // function
+        const add = () => {
+            // 获取元素添加进 items
+            items.value.push(inputValue.value);
+            inputValue.value = "";
+        };
+
+        const remove = (index) => {
+            // 删除单个元素
+            items.value.splice(index, 1);
+        };
+
+        const clear = () => {
+            // 删除所有元素
+            items.value = [];
+        };
+
+        return {
+            items, inputValue,
+            add, remove, clear,
+        }
+    }
+});
+todoapp.mount("#todoapp") 
